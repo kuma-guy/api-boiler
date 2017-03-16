a19c178ada8bd87a3aace41d2139acc39058bf7f
kuma-guy <shingo4092@gmail.com>
2017-03-16 15:08:29 +0900

    Add schema for demo

---
+++ store/schema/1_init.sql
@@ -0,0 +1,17 @@
+-- +migrate Up
+
+CREATE TABLE IF NOT EXISTS books (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255) NULL,
+  author VARCHAR(255) NULL,
+  publishedDate VARCHAR(255) NULL,
+  imageUrl VARCHAR(255) NULL,
+  description TEXT NULL,
+  createdBy VARCHAR(255) NULL,
+  createdById VARCHAR(255) NULL,
+  PRIMARY KEY (id)
+);
+
+-- +migrate Down
+
+DROP TABLE IF EXISTS books;
